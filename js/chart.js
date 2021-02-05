441c30cc7ef9f7a859465a4edc3cba26157fe818
seiwonpark <psw7347@gmail.com>
2021-02-05 22:09:06 +0900

    Added ma20 line

--- js/chart.js
+++ js/chart.js
@@ -1,21 +1,11 @@
 var coinData = [];
 var volData = [];
 var timeData = [];
+var ma5 = [];
+var ma20 = [];
+var ma60 = [];
+var ma120 = [];
 
-// Load Coin Data
-function loadCSV(data) {
-    var rows = data.split(/\r?\n|\r/);
-    for (var i = 1; i < rows.length; i++){
-        var currentLine = rows[i].split(",");
-
-        for (var j = 1; j < currentLine.length; j++){
-            currentLine[j] = parseFloat(currentLine[j]);
-        }
-        volData.push(currentLine.pop());
-        coinData.push(currentLine);
-        timeData.push(currentLine[0]);
-    }
-}
 
 $.ajax({
     url: 'data/bitcoin_historical_data.csv',
@@ -24,51 +14,102 @@ $.ajax({
 }).done(loadCSV);
 
 
+// Load Coin Data
+function loadCSV(data) {
+    var rows = data.split(/\r?\n|\r/);
+    var close = [];
+
+    for (var i = 1; i < rows.length; i++){
+        var ma20_temp = [];
+        var coinData_temp = [];
+        var price_temp = [];
+
+        var currentLine = rows[i].split(",");
+        coinData_temp.push(currentLine[0]);
+
+        for (var j = 1; j < currentLine.length; j++){
+            currentLine[j] = parseFloat(currentLine[j]);
+            price_temp.push(currentLine[j]);
+        }
+        price_temp.pop();
+        coinData_temp.push(price_temp);
+
+        volData.push(currentLine.pop());
+        coinData.push(coinData_temp);
+        timeData.push(currentLine[0]);
+
+        close.push(currentLine[currentLine.length - 1]);
+        if (close.length === 20) {
+            var sum = close.reduce((a, b) => a + b, 0);
+            ma20_temp.push(currentLine[0]);
+            ma20_temp.push(sum/20);
+            ma20.push(ma20_temp);
+            close.shift();
+        }
+    }
+}
+
+
 // Configure candlestick charts
 var candleChartOptions = {
+    series: [{
+        name: 'MA20',
+        type: 'line',
+        data: ma20,
+    },
+        {
+            name: 'candle',
+            type: 'candlestick',
+            data: coinData,
+    }],
     chart: {
-        type: 'candlestick',
+        type: 'line',
         height: 450,
         id: 'candles',
         animations: {
             enabled: false,
-            // speed: 10,
-            // animateGradually: {
-            //     enabled: true,
-            //     delay: 150
-            // },
-            // dynamicAnimation: {
-            //     enabled: true,
-            //     speed: 350
-            // }
         },
         toolbar: {
             autoSelected: 'pan',
             show: false
         },
         zoom: {
-            enabled: true
-        }
+            enabled: true,
+            autoScaleYaxis: false
+        },
     },
-    series: [{
-        data: coinData
-    }],
     noData: {
         text: 'No Data'
     },
     title: {
-        text: 'Bitcoin Price ($)',
+        text: 'Bitcoin Price Chart',
         align: 'left'
+    },
+    stroke: {
+        width: [2, 1],
+        curve: ['smooth', 'straight']
+    },
+    tooltip: {
+        shared: true,
+        x: {
+            format: 'yyyy-MM-dd'
+        },
+        y: {
+
+        }
     },
     xaxis: {
         type: 'datetime',
+        tooltip: {
+            enabled: true
+        },
     },
     yaxis: {
         title: {
-            text: 'BTC',
+            text: 'BTC(USD)',
         },
         tooltip: {
-            enabled: true
+            enabled: false
         },
         decimalsInFloat: 2,
         tickAmount: 5
@@ -119,8 +160,8 @@ var barChartOptions = {
         selection: {
             enabled: true,
             xaxis: {
-                min: "2017-01-01",
-                max: "2017-12-31"
+                min: new Date('01 Jan 2018').getTime(),
+                max: new Date('31 Mar 2018').getTime(),
             },
             fill: {
                 color: '#ccc',
@@ -137,17 +178,12 @@ var barChartOptions = {
     stroke: {
         width: 0
     },
-    labels: timeData,
     xaxis: {
         type: 'datetime',
+        categories: timeData,
     },
     yaxis: {
-        labels: {
-            show: false
-        },
-        tooltip: {
-            enabled: true
-        },
+        tickAmount: 5
     }
 };
 
